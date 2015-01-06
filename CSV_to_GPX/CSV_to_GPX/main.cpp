d0b463d7e680c0ad1dcb68aaf0dd490460d7211c
bergthor13 <bergthor13@ru.is>
2015-01-06 18:32:50 +0000

    Fancified the output file

--- CSV_to_GPX/CSV_to_GPX/main.cpp
+++ CSV_to_GPX/CSV_to_GPX/main.cpp
@@ -49,7 +49,7 @@ GPSPoint parseLine(string line) {
     pointer = nextComma + 1;
     
     nextComma = line.find(delimiter, pointer);
-    cout << line[pointer] << endl;
+    //cout << line[pointer] << endl;
     if (line[pointer] == 'N') {
         
         p.temperature = numeric_limits<double>::min();
@@ -63,35 +63,63 @@ GPSPoint parseLine(string line) {
 }
 
 void logHeader(ofstream &f) {
-    f << "<?xml version=\"1.0\" encoding=\"UTF-8\"?><gpx version=\"1.1\" creator=\"Garmin Connect\" xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd\" xmlns=\"http://www.topografix.com/GPX/1/1\" xmlns:gpxtpx=\"http://www.garmin.com/xmlschemas/TrackPointExtension/v1\" xmlns:gpxx=\"http://www.garmin.com/xmlschemas/GpxExtensions/v3\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><trk> <name>RH508</name><trkseg>";
+    f << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" << endl;
+    f << "<gpx version=\"1.1\" " << endl;
+    f << "     creator=\"Garmin Connect\"" << endl;
+    f << "     xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1" << endl;
+    f << "                         http://www.topografix.com/GPX/1/1/gpx.xsd" << endl;
+    f << "                         http://www.garmin.com/xmlschemas/GpxExtensions/v3" << endl;
+    f << "                         http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd" << endl;
+    f << "                         http://www.garmin.com/xmlschemas/TrackPointExtension/v1" << endl;
+    f << "                         http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd\"" << endl;
+    f << "     xmlns=        \"http://www.topografix.com/GPX/1/1\"" << endl;
+    f << "     xmlns:gpxtpx= \"http://www.garmin.com/xmlschemas/TrackPointExtension/v1\"" << endl;
+    f << "     xmlns:gpxx=   \"http://www.garmin.com/xmlschemas/GpxExtensions/v3\"" << endl;
+    f << "     xmlns:xsi=    \"http://www.w3.org/2001/XMLSchema-instance\">" << endl;
+    f << "    <trk>" << endl;
+    f << "        <name>RH508</name>" << endl;
+    f << "        <trkseg>" << endl;
 }
 
 void logToFile(GPSPoint p, ofstream &f) {
-    if (p.temperature == numeric_limits<double>::min()) {
-        f << "<trkpt lon=\""<< p.longitude << "\" lat=\"" << p.latitude << "\"><ele>" << p.altitude << "</ele><time>" << p.time << "</time></trkpt>";
-    } else {
-        f << "<trkpt lon=\""<< p.longitude << "\" lat=\"" << p.latitude << "\"><ele>" << p.altitude << "</ele><time>" << p.time << "</time><extensions><gpxtpx:TrackPointExtension><gpxtpx:atemp>" << p.temperature << "</gpxtpx:atemp></gpxtpx:TrackPointExtension></extensions></trkpt>";
+    f << "            <trkpt lon=\""<< p.longitude << "\" lat=\"" << p.latitude << "\">" << endl;
+    f << "                <ele>" << p.altitude << "</ele>" << endl;
+    f << "                <time>" << p.time << "</time>" << endl;
+    
+    if (!(p.temperature == numeric_limits<double>::min())) {
+        f << "                <extensions>" << endl;
+        f << "                    <gpxtpx:TrackPointExtension>" << endl;
+        f << "                    <gpxtpx:atemp>" << p.temperature << "</gpxtpx:atemp>" << endl;
+        f << "                    </gpxtpx:TrackPointExtension>" << endl;
+        f << "                </extensions>" << endl;
     }
+
+    f << "            </trkpt>" << endl;
     
 }
 
 void logFooter(ofstream &f) {
-    f << "</trkseg></trk></gpx>";
+    f << "        </trkseg>" << endl;
+    f << "    </trk>" << endl;
+    f << "</gpx>" << endl;
 }
 
+
+
 int main(int argc, const char * argv[]) {
-    freopen("/Volumes/UNTITLED/LOG0053.CSV", "r", stdin);
-    string line;
+    string line, fileLoc = "/Volumes/UNTITLED/LOG0000.CSV";
     GPSPoint now, old;
+    ifstream csvFile(fileLoc);
     ofstream gpxFile;
+
     
     gpxFile.setf(ios::fixed);
     gpxFile.precision(14);
-    gpxFile.open("HELLOTHERE4.gpx");
+    gpxFile.open("HELLOTHERE5.gpx");
     
     logHeader(gpxFile);
-    while (cin.good()) {
-        getline(cin, line);
+    while (csvFile.good()) {
+        getline(csvFile, line);
         now = parseLine(line);
         if ((!now.equals(old) && line != "") && (now.time != old.time)) {
             logToFile(now, gpxFile);
